e9c445bc005b1dfe7c2618b93ad6b6cbef116a17
朱继超 <984065974@qq.com>
2025-08-14 17:52:21 +0800

    Update CallMultiViewController.swift

--- Sources/EaseCallUIKit/Classes/UI/Controllers/CallMultiViewController.swift
+++ Sources/EaseCallUIKit/Classes/UI/Controllers/CallMultiViewController.swift
@@ -108,6 +108,7 @@ open class CallMultiViewController: UIViewController {
         NotificationCenter.default.addObserver(forName: UIApplication.willResignActiveNotification, object: nil, queue: .main) {  _ in
             if #available(iOS 17.4, *),CallKitManager.shared.config.enableVOIP {
                 LiveCommunicationManager.shared.endCall()
+                consoleLogInfo("iOS 17.4及以上系统，开启VOIP模式下，应用进入后台会自动挂断LiveCommunicationManager通话", type: .error)
             }
         }
     }
