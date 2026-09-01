easemob/easemob-callkit-iOS

commit 91a53a76a51c31ad05e5ffa23d1465d58beede7f

callkit special version

EaseCallUIKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'EaseCallUIKit'
-  s.version          = '5.0.0'
+  s.version          = '4.16.4'
   s.summary          = 'A short description of EaseCallUIKit.'
 
 # This description is used to generate tags and improve search results.
@@ -42,6 +42,6 @@ TODO: Add long description of the pod here.
   # s.public_header_files = 'Pod/Classes/**/*.h'
   s.frameworks = 'UIKit', 'Foundation', 'Combine', 'AudioToolbox', 'AVFoundation','AVKit', 'CoreMedia', 'CoreVideo', 'CoreGraphics'
   
-  s.dependency 'HyphenateChat','>=4.19.0'
-  s.dependency 'ShengwangRtcEngine_iOS/RtcBasic', '>=4.6.0'
+  s.dependency 'HyphenateChat','4.16.4'
+  s.dependency 'AgoraRtcEngine_iOS/RtcBasic', '>=4.6.0'
 end

commit b54bfa9dc923028ea0319bab5ffd727591ff8ae2

Restore dev podspec dependencies (HyphenateChat >=4.19.0, ShengwangRtcEngine_iOS)
Commit 91a53a7 accidentally downgraded the dev podspec to the 4.16.4
dependency set; dev sources require the 5.0 SDK line.

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

EaseCallUIKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'EaseCallUIKit'
-  s.version          = '4.16.4'
+  s.version          = '5.0.0'
   s.summary          = 'A short description of EaseCallUIKit.'
 
 # This description is used to generate tags and improve search results.
@@ -42,6 +42,6 @@ TODO: Add long description of the pod here.
   # s.public_header_files = 'Pod/Classes/**/*.h'
   s.frameworks = 'UIKit', 'Foundation', 'Combine', 'AudioToolbox', 'AVFoundation','AVKit', 'CoreMedia', 'CoreVideo', 'CoreGraphics'
   
-  s.dependency 'HyphenateChat','4.16.4'
-  s.dependency 'AgoraRtcEngine_iOS/RtcBasic', '>=4.6.0'
+  s.dependency 'HyphenateChat','>=4.19.0'
+  s.dependency 'ShengwangRtcEngine_iOS/RtcBasic', '>=4.6.0'
 end